d6dc28e0f352d367a2d9b58fe62bb1f4ad3674f2
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-08-08 03:38:45 +0530

    Bug fixes

--- backend/src/configDB/dataSource.ts
+++ backend/src/configDB/dataSource.ts
@@ -7,12 +7,11 @@ import { Favorite } from "../entities/favorite.js";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT!),
-    username: process.env.DB_USERNAME!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_DATABASE!,
-    synchronize: true,
-    entities: [User, Review, Favorite],
+  type: "postgres",
+  url: process.env.DATABASE_URL!,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true,
+  entities: [User, Review, Favorite],
 });
